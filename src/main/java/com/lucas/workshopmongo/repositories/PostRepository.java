165d78415ba4fe7db4657a033857292b5ad571d5
lucas <lucasregoaraujo@gmail.com>
2022-05-04 10:24:58 -0300

    Entidade Post com User aninhado

---
+++ src/main/java/com/lucas/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.lucas.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucas.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>  {
+
+}
