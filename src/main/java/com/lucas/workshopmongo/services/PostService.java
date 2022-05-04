b8f508ff3b1b802943d71dc054a78180a22165fe
lucas <lucasregoaraujo@gmail.com>
2022-05-04 14:18:37 -0300

    Implementado endpoint para retornar posts por id

---
+++ src/main/java/com/lucas/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.lucas.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucas.workshopmongo.domain.Post;
+import com.lucas.workshopmongo.repositories.PostRepository;
+import com.lucas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+		
+	public Post findById(String id){
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	
+
+}
